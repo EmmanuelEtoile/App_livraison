3fbd4c1b823f7d742d7114f649ea57ae8489abf9
Emmanuel <emmanuelnumkam753@gmail.com>
2026-06-21 23:39:39 +0000

    refactor(api): package en minuscules (com.livrapp.api)

--- apps/api/src/main/java/com/livrapp/api/ApiApplication.java
+++ apps/api/src/main/java/com/livrapp/api/ApiApplication.java
@@ -1,4 +1,4 @@
-package com.Livrapp.api;
+package com.livrapp.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- apps/api/src/test/java/com/livrapp/api/ApiApplicationTests.java
+++ apps/api/src/test/java/com/livrapp/api/ApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.Livrapp.api;
+package com.livrapp.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
